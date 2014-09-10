590cb9921d44a2c65af39dde82a1c3771e97c50e
R0CKSTAR <yeahdongcn@gmail.com>
2014-09-10 15:44:00 +0800

    Init commit

---
+++ GCompare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GCompare
+//
+//  Created by R0CKSTAR on 14/9/10.
+//  Copyright (c) 2014年 P.D.Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '8.0'
+
+pod 'OctoKit', '~> 0.5'
+
+# END OF FILE
