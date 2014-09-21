31ad15296d73d26efe98dfc9b03d1370106042ec
R0CKSTAR <yeahdongcn@gmail.com>
2014-09-21 21:20:37 +0800

    Merge branch '获取repo的细节'

---
+++ GCompare/TeamCell.swift
@@ -0,0 +1,24 @@
+//
+//  TeamCell.swift
+//  GCompare
+//
+//  Created by R0CKSTAR on 14/9/21.
+//  Copyright (c) 2014年 P.D.Q. All rights reserved.
+//
+
+import UIKit
+
+class TeamCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
